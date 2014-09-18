9e28e785dbed146a049cd6951ada8795961f2f5f
Martin Anastasov <m4rt00@gmail.com>
2014-09-18 18:36:05 +0300

    Added CategoryViewModel.cs and CategoriesController.cs

---
+++ DefinitelyNotGallery/DNG.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿namespace DNG.Web.Controllers
+{
+    using DNG.Data;
+    using DNG.Web.Models;
+    using System.Linq;
+    using System.Web.Http;
+
+    public class CategoriesController : BaseApiController
+    {
+        public CategoriesController(IDngData data)
+            : base(data)
+        {
+        }
+
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            var categories = this.data
+                .Categories
+                .All()
+                .Select(CategoryViewModel.FromCategory);
+
+            return Ok(categories);
+        }
+    }
+}
---
+++ DefinitelyNotGallery/DNG.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace DNG.Web.Models
+{
+    using DNG.Models;
+    using System;
+    using System.Linq.Expressions;
+
+    public class CategoryViewModel
+    {
+        public static Expression<Func<Category, CategoryViewModel>> FromCategory
+        {
+            get
+            {
+                return c => new CategoryViewModel()
+                {
+                    Id = c.CategoryID,
+                    Name = c.Name,
+                    Description = c.Description
+                };
+            }
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
