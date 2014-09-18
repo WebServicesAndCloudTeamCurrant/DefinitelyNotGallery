2f33e92cc53f6ba3b44c9ba508f590cff5c9cefb
TodorVyagov <rambo007@abv.bg>
2014-09-18 11:30:53 +0300

    Added cross-origin support

--- DefinitelyNotGallery/DNG.Web/App_Start/WebApiConfig.cs
+++ DefinitelyNotGallery/DNG.Web/App_Start/WebApiConfig.cs
@@ -14,6 +14,7 @@
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            config.EnableCors();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
