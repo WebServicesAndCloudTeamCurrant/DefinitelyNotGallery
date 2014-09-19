92c1b4333994007be4cccd8cb69c01e3d5b822a6
Martin Anastasov <m4rt00@gmail.com>
2014-09-19 11:02:52 +0300

    Added some actions in CategoriesController

--- DefinitelyNotGallery/DNG.Web/Controllers/CategoriesController.cs
+++ DefinitelyNotGallery/DNG.Web/Controllers/CategoriesController.cs
@@ -1,10 +1,11 @@
 ﻿namespace DNG.Web.Controllers
 {
+    using System.Linq;
+    using System.Web.Http;
+
     using DNG.Data;
     using DNG.Models;
     using DNG.Web.Models;
-    using System.Linq;
-    using System.Web.Http;
 
     public class CategoriesController : BaseApiController
     {
@@ -50,12 +51,49 @@
                 return BadRequest(ModelState);
             }
 
-            var newCategory = new Category()
+            var newCategory = new Category
             {
                 Name = category.Name
             };
 
             this.data.Categories.Add(newCategory);
+            this.data.SaveChanges();
+
+            category.Id = newCategory.CategoryID;
+            return Ok(category);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(int id, CategoryViewModel category)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingCategory = this.data.Categories.All().FirstOrDefault(a => a.CategoryID == id);
+            if (existingCategory == null)
+            {
+                return BadRequest("Such category does not exists!");
+            }
+
+            existingCategory.Name = category.Name;
+            this.data.SaveChanges();
+
+            category.Id = id;
+            return Ok(category);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var existingCategory = this.data.Categories.All().FirstOrDefault(a => a.CategoryID == id);
+            if (existingCategory == null)
+            {
+                return BadRequest("Such category does not exists!");
+            }
+
+            this.data.Categories.Delete(existingCategory);
             this.data.SaveChanges();
 
             return Ok();
